8e07d7eecc0feb9e435a04c96d2715c1f4aef90a
Arpan Das <arpan.das@klythe.com>
2023-12-22 21:09:15 +0530

    personalized greeting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [personalGreating,setPersonalGreating] = useState()
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name:</label>
+        <input type="text" onChange={(e)=> setPersonalGreating(e.target.value)}></input>
+        <p>{personalGreating}</p>
     </div>
   )
 }
