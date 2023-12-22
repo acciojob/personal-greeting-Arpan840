9983d0366599fd3f252df1e204fd9ee2cc4f4df8
Arpan Das <arpan.das@klythe.com>
2023-12-22 21:17:04 +0530

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,17 @@ import './../styles/App.css';
 
 const App = () => {
   let [personalGreating,setPersonalGreating] = useState()
+  let handelChange=(e)=>{
+    
+   setPersonalGreating(e.target.value)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
         <label>Enter your name:</label>
-        <input type="text" onChange={(e)=> setPersonalGreating(e.target.value)}></input>
+        <br></br>
+        <input type="text" onChange={(e)=>handelChange(e)}></input>
         <p>{personalGreating}</p>
     </div>
   )
